72a1d692c52315b9b0758a7388ed303aa4e3d58b
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 02:36:30 +0900

    :factory: (dotfiles) [automated] sync dotfiles

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -50,7 +50,7 @@ module.exports = {
     webGLRenderer: true,
     /// TODO
     hyperTabsMove: {
-      moveLeft: 'command+[',
+      moveLeft: 'command+left',
     },
     StarWarsTheme: {
       character: 'emperor', // Define your favorite star wars character
