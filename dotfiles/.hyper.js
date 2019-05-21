98a995f0dda489a5a9255d7407319e3f4174ede5
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 02:27:27 +0900

    :factory: (dotfiles) [automated] sync dotfiles

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -54,7 +54,7 @@ module.exports = {
       moveLeft: ['command', 'right'],
     },
     StarWarsTheme: {
-      character: 'darth-vader', // Define your favorite star wars character
+      character: 'finn-stormtrooper', // Define your favorite star wars character
       lightsaber: 'true', // Activate your theme's lightsaber mode
       unibody: 'false', // Define the color of the Hyper window header
       avatar: 'true' // Activate your theme's background avatar
