95fa42128b3f1a083cfea256b4139ea9ef82cc80
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 03:26:27 +0900

    complete work tmux

--- utils/work_tmux.sh
+++ utils/work_tmux.sh
@@ -1,22 +1,17 @@
 session="WORK"
+
 tmux start-server
 
 tmux new-session -d -s $session
 
-tmux selectp -t 1 
-tmux send-keys "gotop" C-m 
+tmux send-keys "gotop" C-m
 
-tmux send-keys "neofetch" C-m 
+tmux split-window -h
 
-tmux selectp -t 2
+tmux send-keys "neofetch" C-m
 
-tmux selectp -t 3
+tmux split-window
+
 tmux send-keys "htop" C-m 
 
 tmux selectp -t 1
-
-tmux new-window -t $session:1 -n FREE
-
-tmux select-window -t $session:0
-
-tmux attach-session -t $session
