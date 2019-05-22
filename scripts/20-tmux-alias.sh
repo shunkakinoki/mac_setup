0d5d5e273966f8c07eada204571b176f913cdfe3
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 11:41:47 +0900

    created function TXDASH

--- scripts/20-tmux-alias.sh
+++ scripts/20-tmux-alias.sh
@@ -1,9 +1,11 @@
 ### TMUX ALIAS
-alias TXDASH="tmux new -s DASH \; \
-    send-keys 'gotop' C-m \; \
-    split-window -h \; \
-    send-keys 'neofetch' C-m \; \
-    split-window -v \;"
+function TXDASH {
+    tmux new -s DASH \;
+    send-keys 'gotop' C-m \;
+    split-window -h \;
+    send-keys 'neofetch' C-m \;
+    split-window -v \;
+}
 
 alias TXKILLDASH="tmux kill-session -t DASH"
 
