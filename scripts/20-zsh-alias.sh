75ef69736ae912e45f1343d43524090431baf582
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 11:19:50 +0900

    add zsh alias

---
+++ scripts/20-zsh-alias.sh
@@ -0,0 +1,6 @@
+### ZSH ALIAS
+
+alias RESET_MACSETUP='rm -rf ~/.mac_setup && curl https://raw.githubusercontent.com/shunkakinoki/mac_setup/master/install.sh | bash'
+alias RELOAD='source ~/.zshrc'
+alias CHANGE='code ~/.zshrc'
+alias CHANGEALL='code ~/.mac_setup'
