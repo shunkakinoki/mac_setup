5740c60b9d672a6ae5be8c35493af49747fdbf9d
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-22 10:30:14 +0900

    update emoji

--- scripts/10-configure-zsh.sh
+++ scripts/10-configure-zsh.sh
@@ -2,12 +2,7 @@
 
 if ! command -v zsh > /dev/null 2>&1; then
   echo "Setting zsh as login shell..."
-  chsh -s /bin/zsh
-fi
-
-if [[ ! -d ~/.oh-my-zsh ]]; then
-  echo "Installing oh-my-zsh..."
-  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  chsh -s /usr/local/bin/zsh
 fi
 
 if [[ ! -a ~/.zshrc ]]; then
