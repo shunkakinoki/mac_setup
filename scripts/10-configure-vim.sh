b5a382f3ea484c7f5683611e150092c8b31a9fd6
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-24 08:04:50 +0900

    configuring vim

--- scripts/10-configure-vim.sh
+++ scripts/10-configure-vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Configuring vim"
-rm -f ~/.vim_runtime
+rm -rf ~/.vim_runtime
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "Done."
