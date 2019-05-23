8ed966a7f544aad0f349ed6e344aaebb7a10833f
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-24 08:02:01 +0900

    added vim myconfig

--- scripts/10-configure-vim.sh
+++ scripts/10-configure-vim.sh
@@ -5,3 +5,8 @@ rm -f ~/.vim_runtime
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "Done."
+
+echo "Configuring vim myconfig"
+rm -f ~/.vim_runtime/my_configs.vim
+ln -s ~/.mac_setup/dotfiles/.my_configs.vim ~/.vim_runtime/my_configs.vim
+echo "Done."
