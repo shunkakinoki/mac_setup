a888a85fa5dcc1c049f519fca20e6e49b50c2227
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-29 17:58:59 +0900

    changed ln to cp

--- scripts/10-configure-joplin.sh
+++ scripts/10-configure-joplin.sh
@@ -2,5 +2,5 @@
 
 echo "Configuring Joplin"
 rm -f ~/.config/joplin/keymap.json
-ln -s ~/.mac_setup/dotfiles/.joplin.json ~/.config/joplin/keymap.json
+cp ~/.mac_setup/dotfiles/.joplin.json ~/.config/joplin/keymap.json
 echo "Done."
