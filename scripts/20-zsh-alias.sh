eecfe2a0f369f7aa744086857c4bfb8a637dd755
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-19 17:47:58 +0900

    corrected alias

--- scripts/20-zsh-alias.sh
+++ scripts/20-zsh-alias.sh
@@ -1,6 +1,5 @@
 ### ZSH ALIAS
 
-alias python='python3'
 alias RESET_MACSETUP='rm -rf ~/.mac_setup && curl https://raw.githubusercontent.com/shunkakinoki/mac_setup/master/install.sh | bash'
 alias RELOAD='source ~/.zshrc'
 alias CHANGE='code ~/.zshrc'
