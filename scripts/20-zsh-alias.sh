602bf2c7a63337e474346b504e104b1cf2f53a25
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-25 19:05:03 +0900

    update screensaver aliases

--- scripts/20-zsh-alias.sh
+++ scripts/20-zsh-alias.sh
@@ -4,3 +4,6 @@ alias RESET_MACSETUP='rm -rf ~/.mac_setup && curl https://raw.githubusercontent.
 alias RELOAD='source ~/.zshrc'
 alias CHANGE='code ~/.zshrc'
 alias CHANGEALL='code ~/.mac_setup'
+
+alias SSOFF='defaults -currentHost write com.apple.screensaver idleTime 0'
+alias SSON='defaults -currentHost write com.apple.screensaver idleTime 60'
