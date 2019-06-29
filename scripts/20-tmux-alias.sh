ad328104871a2a96619b7c24cbbb1b03117b27ed
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-29 23:03:48 +0900

    update 20 scripts

--- scripts/20-tmux-alias.sh
+++ scripts/20-tmux-alias.sh
@@ -2,9 +2,6 @@
 
 ### TMUX ALIAS
 alias TXDASH="source ~/.mac_setup/scripts/50-tmux-dash.sh"
-
 alias TXKILLDASH="tmux kill-session -t DASH"
-
 alias TXLS="tmux ls"
-
 alias TXKILLALL="tmux kill-server"
--- scripts/20-zsh-alias.sh
+++ scripts/20-zsh-alias.sh
@@ -1,10 +1,12 @@
 ### ZSH ALIAS
 
+### MAC SETUP
 alias RESET_MACSETUP='rm -rf ~/.mac_setup && curl https://raw.githubusercontent.com/shunkakinoki/mac_setup/master/install.sh | bash'
 alias RELOAD='source ~/.zshrc'
 alias CHANGE='code ~/.zshrc'
 alias CHANGEALL='code ~/.mac_setup'
 
+### SCREEN SAVER
 alias SSOFF='defaults -currentHost write com.apple.screensaver idleTime 0'
 alias SSON='defaults -currentHost write com.apple.screensaver idleTime 60'
 alias SST='defaults -currentHost read com.apple.screensaver idleTime'
