6e9d902a88de5b2aadb9470a3de2a80db52abc1a
Shun Kakinoki <shunkakinoki@gmail.com>
2019-07-08 03:26:49 +0900

    :heart_eyes: (dotfiles) [automated] sync dotfiles

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -50,11 +50,11 @@ module.exports = {
     macOptionSelectionMode: 'vertical',
     webGLRenderer: true,
 
-    hyperBorder: {
-      animate: true,
-      borderColors: ['#fc1da7', '#fba506'],
-      borderWidth: '3px'
-    },
+    // hyperBorder: {
+    //   animate: true,
+    //   borderColors: ['#fc1da7', '#fba506'],
+    //   borderWidth: '3px'
+    // },
 
     /// TODO
     hyperTabsMove: {
