e8350e0e91c569b50a22d614d2bfc2c23a1d7a01
Shun Kakinoki <shunkakinoki@gmail.com>
2019-07-12 06:16:29 +0900

    updated scripts dir

--- scripts/10-configure-c++.sh
+++ scripts/10-configure-c++.sh
@@ -2,6 +2,6 @@
 
 if [[ ! -a /local/include/bits/stdc++.h ]]; then
   echo "Linking bits/stdc++.h"
-  ln -s ~/.mac_setup/bits/stdc++.h /local/include/bits/stdc++.h
+  ln -s ~/.mac_setup/scripts/stdc++.h /local/include/bits/stdc++.h
   echo "Done."
 fi
